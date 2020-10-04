c16a78356832ef7df9b5faf464c91f1aa4ffb3e6
jiianxue <1048725797@qq.com>
2020-10-04 19:03:15 +0800

    1

---
+++ precache-manifest.a4822add255065502add19b7dad61c60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd99a269bc8e53fde193fd95274c162",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "./static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e102b4215e56049551",
+    "url": "./static/js/main.4a3a0a5d.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
